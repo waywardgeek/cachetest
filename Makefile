2e7c4634d9886e2ef9e413efd9114d9b8fb5b93b
Bill Cox <waywardgeek@ciphershed.org>
2014-08-28 10:20:15 -0400

    Updated to test more cases

--- Makefile
+++ Makefile
@@ -1,2 +1,19 @@
-cachetest: cachetest.c
-	gcc -Wall -march=native -std=gnu99 -g -o cachetest cachetest.c
+all: cachetest16 cachetest64 cachetest1024 cachetest4096 cachetest16384
+
+clean:
+	rm cachetest16 cachetest64 cachetest1024 cachetest4096 cachetest16384
+
+cachetest16: cachetest.c
+	gcc -Wall -march=native -std=gnu99 -O3 -DBLOCK_EXP=0 -o cachetest16 cachetest.c
+
+cachetest64: cachetest.c
+	gcc -Wall -march=native -std=gnu99 -O3 -DBLOCK_EXP=2 -o cachetest64 cachetest.c
+
+cachetest1024: cachetest.c
+	gcc -Wall -march=native -std=gnu99 -O3 -DBLOCK_EXP=6 -o cachetest1024 cachetest.c
+
+cachetest4096: cachetest.c
+	gcc -Wall -march=native -std=gnu99 -O3 -DBLOCK_EXP=8 -o cachetest4096 cachetest.c
+
+cachetest16384: cachetest.c
+	gcc -Wall -march=native -std=gnu99 -O3 -DBLOCK_EXP=10 -o cachetest16384 cachetest.c
--- cachetest.c
+++ cachetest.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+static inline uint32_t fastrand() { 
+	static uint32_t seed = 12345;
+    //seed = (214013U*seed+2531011U); 
+    //return (seed>>16)&0x7FFF; 
+	seed *= 3;
+	return seed;
+}
+
 static void time_ms(struct timespec *start, struct timespec *end, double *ms)
 {
     double start_ms, end_ms;
@@ -13,36 +21,104 @@ static void time_ms(struct timespec *start, struct timespec *end, double *ms)
     *ms = end_ms - start_ms;
 }
 
-// Perform random 8 byte reads and writes throughout 2^memSize memory, for 2^totalData
-static void randomReadWrite(uint64_t *mem, uint32_t memSize, uint32_t totalData) {
+// Perform random writes throughout memory.
+static void randomWrite(__uint128_t *mem, uint32_t memExp, uint32_t dataExp, uint32_t blockExp) {
     struct timespec start, end;
     double ms;
-    uint32_t data = 12345;
-    uint32_t address = 1234;
-    uint32_t memWords = 1U << (memSize - 2);
-    uint32_t dataWords = 1U << (totalData - 2);
+    __uint128_t data = 12345;
+    uint32_t address = 1;
+	uint32_t blockWords = 1 << blockExp;
+    uint32_t memBlocks = 1U << (memExp - 4 - blockExp);
+    uint32_t dataBlocks = 1U << (dataExp - 4 - blockExp);
     clock_gettime(CLOCK_MONOTONIC, &start);
-    for(uint32_t i = 0; i < dataWords; i++) {
-        address += 104729;
-        uint32_t index = address & (memWords-1);
-        data += mem[index] + address;
-        mem[index] = data;
+    for(uint32_t i = 0; i < dataBlocks; i++) {
+		address += fastrand();
+        uint32_t index = (address & (memBlocks-1)) << blockExp;
+        for(uint32_t j = 0; j < blockWords; j++) {
+			data += address;
+			mem[index + j] = data;
+		}
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
     time_ms(&start, &end, &ms);
-    printf("Access time for random read then write to %u memory is %gns\n", 1 << memSize, 1000000*ms/dataWords);
+	double nsPerBlock = 1000000*ms/dataBlocks;
+	double nsPerByte = nsPerBlock/(16*blockWords);
+    printf("For random writes to %u memory: %gns per block, or %gns per byte\n", 1 << memExp, nsPerBlock, nsPerByte);
+}
+
+// Perform random reads throughout memory.
+static void randomRead(__uint128_t *mem, uint32_t memExp, uint32_t dataExp, uint32_t blockExp) {
+    struct timespec start, end;
+    double ms;
+    __uint128_t data = 12345;
+    uint32_t address = 1234;
+	uint32_t blockWords = 1 << blockExp;
+    uint32_t memBlocks = 1U << (memExp - 4 - blockExp);
+    uint32_t dataBlocks = 1U << (dataExp - 4 - blockExp);
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    uint32_t index = 0;
+    for(uint32_t i = 0; i < dataBlocks; i++) {
+        address += fastrand();
+        index = (address & (memBlocks-1)) << blockExp;
+        for(uint32_t j = 0; j < blockWords; j++) {
+			data += mem[index + j];
+		}
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time_ms(&start, &end, &ms);
+	mem[index] = data; // Needed so the optimizer doesn't just drop the loop
+	double nsPerBlock = 1000000*ms/dataBlocks;
+	double nsPerByte = nsPerBlock/(16*blockWords);
+    printf("For random reads from %u memory: %gns per block, or %gns per byte\n", 1 << memExp, nsPerBlock, nsPerByte);
+}
+
+// Perform random read then writes throughout memory.
+static void randomReadWrite(__uint128_t *mem, uint32_t memExp, uint32_t dataExp, uint32_t blockExp) {
+    struct timespec start, end;
+    double ms;
+    __uint128_t data = 12345;
+    uint32_t address = 1234;
+	uint32_t blockWords = 1 << blockExp;
+    uint32_t memBlocks = 1U << (memExp - 4 - blockExp);
+    uint32_t dataBlocks = 1U << (dataExp - 4 - blockExp);
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for(uint32_t i = 0; i < dataBlocks; i++) {
+        address += fastrand();
+        uint32_t index = (address & (memBlocks-1)) << blockExp;
+        for(uint32_t j = 0; j < blockWords; j++) {
+			data += mem[index + j] + address;
+			mem[index + j] = data;
+		}
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time_ms(&start, &end, &ms);
+	double nsPerBlock = 1000000*ms/dataBlocks;
+	double nsPerByte = nsPerBlock/(16*blockWords);
+    printf("For readom read-then-write from %u memory: %gns per block, or %gns per byte\n", 1 << memExp, nsPerBlock, nsPerByte);
 }
 
 int main() {
-    uint64_t *mem = malloc(1ULL << 30);
-
+    __uint128_t *mem = calloc(1ULL << 30, 1);
+	uint32_t blockExp = BLOCK_EXP; // Define this in Makefile to set memory size.
+    printf("Access times for random read then write of %u byte blocks\n", 1 << (blockExp + 4));
     // 16KiB random read/write test
-    randomReadWrite(mem, 14, 30);
+    randomWrite(mem, 14, 30, blockExp);
+    randomRead(mem, 14, 30, blockExp);
+    randomReadWrite(mem, 14, 30, blockExp);
+	printf("\n");
     // 128KiB random read/write test
-    randomReadWrite(mem, 17, 30);
+    randomWrite(mem, 17, 30, blockExp);
+    randomRead(mem, 17, 30, blockExp);
+    randomReadWrite(mem, 17, 30, blockExp);
+	printf("\n");
     // 4MiB random read/write test
-    randomReadWrite(mem, 24, 30);
+    randomWrite(mem, 22, 30, blockExp);
+    randomRead(mem, 22, 30, blockExp);
+    randomReadWrite(mem, 22, 30, blockExp);
+	printf("\n");
     // 1GiB random read/write test
-    randomReadWrite(mem, 30, 30);
+    randomWrite(mem, 30, 30, blockExp);
+    randomRead(mem, 30, 30, blockExp);
+    randomReadWrite(mem, 30, 30, blockExp);
     return 0;
 }
